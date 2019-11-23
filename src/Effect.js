7310015101ba37dd6009d7609c5bf55c4d126613
brunobc <brunobezerrachaves@gmail.com>
2019-11-23 11:28:14 -0300

    Use effect listener

--- src/Effect.js
+++ src/Effect.js
@@ -5,20 +5,33 @@ export default function Effect() {
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(
-        "https://api.github.com/users/brunobc/repos"
-      );
+      const response = await fetch("https://api.github.com/users/brunobc/repos");
       const data = await response.json();
-      console.log(data);
-
       setRepositories(data);
     }
     fetchData();
   }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    document.title = `${filtered.length} favorites`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo =>
+      repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    );
+    setRepositories(newRepositories);
+  }
+
   return (
     <ul>
       {repositories.map(repo => (
-        <li key={repo.id}>{repo.name}</li>
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
       ))}
     </ul>
   );
